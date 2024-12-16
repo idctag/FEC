a5a0325c5341c5d42a2fa1f213f0b03410e7a136
idctag <idctag@Battulgas-MacBook-Air.local>
2024-12-16 19:36:23 +0800

    carousel final redo i hope

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -7,9 +7,9 @@ import {
   DialogBody,
   DialogHeader,
 } from "@material-tailwind/react";
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { motion, useAnimationControls } from "framer-motion";
 
 const HeroLeft = ({ title, subtitle }: { title: string; subtitle: string }) => {
   const titleParts = title.split(/(\[.*?\])/);
@@ -61,11 +61,19 @@ const HeroLeft = ({ title, subtitle }: { title: string; subtitle: string }) => {
 const HeroRight = ({ advantage }: { advantage: Advantage[] }) => {
   const [open, setOpen] = React.useState(false);
   const [index, setIndex] = React.useState(0);
+  const controls = useAnimationControls();
+  const handleHover = () => {
+    controls.start("open");
+  };
 
+  const handleHoverEnd = () => {
+    controls.start("initial");
+  };
   const handleOpen = () => setOpen(!open);
   return (
-    <div className="flex md:w-1/2 h-[60vh] max-w-[550px] md:h-[80%]">
+    <div className="flex md:w-1/2 h-[60vh] w-full max-w-[550px] md:h-[80%]">
       <Carousel
+        autoplay
         className="md:rounded-3xl"
         loop
         navigation={({ setActiveIndex, activeIndex, length }) => (
@@ -82,25 +90,91 @@ const HeroRight = ({ advantage }: { advantage: Advantage[] }) => {
         )}
       >
         {advantage.map((item, idx) => (
-          <div
+          <motion.div
+            onHoverStart={handleHover}
+            onHoverEnd={handleHoverEnd}
             key={idx}
             onClick={() => {
               setIndex(idx);
               handleOpen();
             }}
-            className="relative size-full"
+            className="object-fill size-full absolute overflow-hidden"
           >
-            <Image src={item.image.url!} priority alt="" fill sizes="auto" />
-            <div className="absolute inset-0 grid h-full w-full place-items-center hover:bg-black/60 hover:cursor-pointer"></div>
-          </div>
+            <motion.img
+              src={item.image.url!}
+              variants={{
+                initial: {
+                  scale: 1,
+                },
+                open: {
+                  scale: 1.1,
+                },
+              }}
+              transition={{ duration: 0.5, ease: "easeInOut" }}
+              alt=""
+              className="object-fill size-full"
+              initial="initial"
+              animate={controls}
+            />
+            <motion.div className="absolute inset-0 grid h-full w-full place-items-center group hover:bg-black/60 hover:cursor-pointer">
+              <motion.p
+                variants={{
+                  initial: {
+                    opacity: 0,
+                    x: -10,
+                  },
+                  open: {
+                    opacity: 1,
+                    x: 0,
+                  },
+                }}
+                transition={{
+                  duration: 0.5,
+                  ease: "easeInOut",
+                }}
+                className="text-white text-3xl"
+                initial="initial"
+                animate={controls}
+              >
+                Click for More
+              </motion.p>
+
+              <motion.p
+                variants={{
+                  initial: {
+                    opacity: 0,
+                    x: -10,
+                  },
+                  open: {
+                    opacity: 1,
+                    x: 0,
+                  },
+                }}
+                transition={{
+                  duration: 0.5,
+                  ease: "easeInOut",
+                }}
+                className="text-white px-4 text-xl md:text-3xl"
+                initial="initial"
+                animate={controls}
+              >
+                {item.title}
+              </motion.p>
+            </motion.div>
+          </motion.div>
         ))}
       </Carousel>
-      <Dialog className="p-12 text-center" open={open} handler={handleOpen}>
+      <Dialog
+        size="lg"
+        className="p-2 md:p-12 text-center"
+        open={open}
+        handler={handleOpen}
+      >
         <DialogHeader>
-          <p className="text-[40px]">{advantage[index].title}</p>
+          <p className="text-[20px] md:text-[30px]">{advantage[index].title}</p>
         </DialogHeader>
         <DialogBody>
-          <p className="text-[30px]">{advantage[index].sub}</p>
+          <p className="text-[16px] md:text-[20px]">{advantage[index].sub}</p>
         </DialogBody>
       </Dialog>
     </div>
