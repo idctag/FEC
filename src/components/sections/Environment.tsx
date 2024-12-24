679980be449d097bf7f16ea1f0b31aeec21aa1ae
idctag <idctag@Battulgas-MacBook-Air.local>
2024-12-24 19:46:36 +0800

    animation respoinsive

--- src/components/sections/Environment.tsx
+++ src/components/sections/Environment.tsx
@@ -10,7 +10,10 @@ const Environment = ({ image, title, subtitle, desc }: EnvironmentProps) => {
     <div className="flex gap-12 flex-col items-center relative my-20 max-w-bigscrn">
       <div>
         <div className="flex">
-          <Lottie animationData={objects} className="size-[100px]" />
+          <Lottie
+            animationData={objects}
+            className="size-[70px] left-10 2xl:size-[100px] absolute 2xl:static"
+          />
 
           <p className="text-[2.5rem] font-semibold 2xl:text-[4rem] text-center">
             {title}
@@ -19,7 +22,7 @@ const Environment = ({ image, title, subtitle, desc }: EnvironmentProps) => {
         <div className="h-1 w-[12rem] mx-auto rounded-3xl bg-gradient-to-r to-primary from-secondary" />
       </div>
       <div className="flex flex-col 2xl:flex-row items-center md:p-12 gap-8">
-        <div className="flex flex-col relative gap-12 size-full 2xl:w-1/2 ">
+        <div className="flex flex-col relative text-center 2xl:text-left gap-12 size-full 2xl:w-1/2 p-2">
           <p className="text-[1.3rem] 2xl:text-[2.5rem]">{subtitle}</p>
           <p className="2xl:text-[1.4rem] text-gray-500">{desc}</p>
         </div>
