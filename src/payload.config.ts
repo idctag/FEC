300bb77059b8be3b539eb8a0560d3ba0cb2d6423
idctag <idctag@Battulgas-MacBook-Air.local>
2024-12-27 01:56:18 +0800

    package update

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,11 +1,11 @@
 // storage-adapter-import-placeholder
-import { postgresAdapter } from "@payloadcms/db-postgres";
 import { payloadCloudPlugin } from "@payloadcms/payload-cloud";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import path from "path";
 import { buildConfig } from "payload";
 import { fileURLToPath } from "url";
 import sharp from "sharp";
+import { postgresAdapter } from "@payloadcms/db-postgres";
 
 import { Users } from "./collections/Users";
 import { Media } from "./collections/Media";
