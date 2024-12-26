2960c8fa3b8e738d7b7ddaa77f0566e6de9469a6
idctag <idctag@Battulgas-MacBook-Air.local>
2024-12-26 23:09:28 +0800

    local db

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -89,7 +89,6 @@ export interface User {
 export interface Media {
   id: number;
   alt: string;
-  _key?: string | null;
   updatedAt: string;
   createdAt: string;
   url?: string | null;
@@ -343,7 +342,6 @@ export interface UsersSelect<T extends boolean = true> {
  */
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
-  _key?: T;
   updatedAt?: T;
   createdAt?: T;
   url?: T;
